5677b868a1678bc000164730d295c2a6cb933a32
Julian Mojico <jmojico@gmail.com>
2022-08-16 04:28:36 -0300

    title animado entero

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -5,8 +5,8 @@ import './styles.css'
 
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
-const to = (i) => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
-const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
+const to = (i) => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 + 6000 })
+const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -2000 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
 const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
