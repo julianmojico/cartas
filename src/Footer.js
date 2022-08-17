2483bf56a71ac472ce348aa8316dabe31778f242
Julian Mojico <jmojico@gmail.com>
2022-08-17 02:14:43 -0300

    responsive > 700px

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { BsInstagram } from 'react-icons/bs';
-
+import { BsInstagram } from 'react-icons/bs'
+import { AiOutlineCoffee } from 'react-icons/ai'
 
 const Footer = ({ }) => {
     return (
         <div className="footer">
-            <BsInstagram color="white" size="small" />
+            <a href="https://cafecito.app/tirandoaverde" target="_blank">
+                <AiOutlineCoffee color="white" size="medium"/>
+            </a>
+            <a href="https://www.instagram.com/tirandoaverde_arte/" target="_blank">
+                <BsInstagram color="white" size="medium"/>
+            </a>
         </div>
     )
 }
