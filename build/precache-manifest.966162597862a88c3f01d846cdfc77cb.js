8137e2bd42f6716d646e19a55cb74842d8433950
Julian Mojico <jmojico@gmail.com>
2022-08-17 02:19:23 -0300

    build1

---
+++ build/precache-manifest.966162597862a88c3f01d846cdfc77cb.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c143d1ba8951caf22d039ce9cabb9947",
+    "url": "/s/new/index.html"
+  },
+  {
+    "revision": "235174e9a1c7ea3a86e8",
+    "url": "/s/new/static/css/main.b5f1de40.chunk.css"
+  },
+  {
+    "revision": "38841682146f9b15df50",
+    "url": "/s/new/static/js/2.46fb058f.chunk.js"
+  },
+  {
+    "revision": "235174e9a1c7ea3a86e8",
+    "url": "/s/new/static/js/main.142e0b26.chunk.js"
+  },
+  {
+    "revision": "85945ddbca995500257a",
+    "url": "/s/new/static/js/runtime~main.01695a31.js"
+  },
+  {
+    "revision": "226daa18413a2ac59875f7634fd7b732",
+    "url": "/s/new/static/media/El_Carro.226daa18.png"
+  },
+  {
+    "revision": "5d130ea24557449f027953149cd1e25a",
+    "url": "/s/new/static/media/El_Diablo.5d130ea2.png"
+  },
+  {
+    "revision": "d7b94d5f2f7e53c49345ec6164c9bbb0",
+    "url": "/s/new/static/media/El_Emperador.d7b94d5f.png"
+  },
+  {
+    "revision": "e8d4c58d61e71ee9347fd1353f7cd80b",
+    "url": "/s/new/static/media/El_Juicio.e8d4c58d.png"
+  },
+  {
+    "revision": "c33311d27ca5381a0b18005afbb41645",
+    "url": "/s/new/static/media/El_Mundo.c33311d2.jpg"
+  },
+  {
+    "revision": "c67d8af34104cc1d6fe8ab38166d78ce",
+    "url": "/s/new/static/media/El_Sol.c67d8af3.png"
+  },
+  {
+    "revision": "487c8335f88d2c76fe24585e3d5cff0d",
+    "url": "/s/new/static/media/El_Sumo_Sacerdote.487c8335.png"
+  },
+  {
+    "revision": "1b7a622863b21e73571c1b81c62475d4",
+    "url": "/s/new/static/media/La_Colgada.1b7a6228.png"
+  },
+  {
+    "revision": "8d9bc293696089fcefd4d26d1eb828fd",
+    "url": "/s/new/static/media/La_Emperatriz.8d9bc293.png"
+  },
+  {
+    "revision": "bc0aeffc11f545276fec252987d464c7",
+    "url": "/s/new/static/media/La_Ermitania.bc0aeffc.png"
+  },
+  {
+    "revision": "20e4e82de328a7b1922a3c6ecc69a360",
+    "url": "/s/new/static/media/La_Estrella.20e4e82d.png"
+  },
+  {
+    "revision": "4bdfa0d80de745fdb0426497e6828c41",
+    "url": "/s/new/static/media/La_Fuerza.4bdfa0d8.png"
+  },
+  {
+    "revision": "96168849061db2a43159baaee8f39bda",
+    "url": "/s/new/static/media/La_Justicia.96168849.png"
+  },
+  {
+    "revision": "9c31a0691fb23037bbb82c2654f46579",
+    "url": "/s/new/static/media/La_Loca.9c31a069.png"
+  },
+  {
+    "revision": "9dcaf4495de5a51990e1e03cfc848e22",
+    "url": "/s/new/static/media/La_Luna.9dcaf449.png"
+  },
+  {
+    "revision": "c56e72cd7bc43717786570a8e2091b26",
+    "url": "/s/new/static/media/La_Maga.c56e72cd.png"
+  },
+  {
+    "revision": "265ea612c5b8bc63ac1a359bdf6b5230",
+    "url": "/s/new/static/media/La_Muerte.265ea612.png"
+  },
+  {
+    "revision": "437719ec700c70ff6018051d53052c46",
+    "url": "/s/new/static/media/La_Rueda.437719ec.png"
+  },
+  {
+    "revision": "b9d2a16370a4ad24c3ee8ab0d16898d3",
+    "url": "/s/new/static/media/La_Templanza.b9d2a163.png"
+  },
+  {
+    "revision": "0e9ec460b99016c21eb6d5ca0ce1b3aa",
+    "url": "/s/new/static/media/La_Torre.0e9ec460.png"
+  },
+  {
+    "revision": "ed7f1cb5122eec5229903b0410b37375",
+    "url": "/s/new/static/media/Lxs_Enamoradxs.ed7f1cb5.png"
+  },
+  {
+    "revision": "2532e8308ed1d864068a9d6c970096e6",
+    "url": "/s/new/static/media/Sacerdotiza.2532e830.png"
+  },
+  {
+    "revision": "bf1d91ec4c9d4f315cc19b524a2c1738",
+    "url": "/s/new/static/media/title.bf1d91ec.svg"
+  }
+]);
